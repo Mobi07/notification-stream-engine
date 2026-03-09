aed0f2b6b9651f0c3a2e94c3be327b53be2dacd8
Mobi07 <mobasshir.pacefin@gmail.com>
2026-03-10 00:00:12 +0530

    initialize api service

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,1 +1,10 @@
 package main
+
+import "github.com/Mobi07/notification-stream-engine.git/pkg/logger"
+
+func main() {
+	logger.Init()
+	defer logger.Sync()
+
+	logger.Log.Info("API service started")
+}
