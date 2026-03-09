b4e7f840ca8d022e8660d9b20f434784af223fc8
Mobi07 <mobasshir.pacefin@gmail.com>
2026-03-10 00:19:27 +0530

    add worker and api server main code

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,10 +1,25 @@
 package main
 
-import "github.com/Mobi07/notification-stream-engine.git/pkg/logger"
+import (
+	"github.com/Mobi07/notification-stream-engine.git/internal/broker/rabbitmq"
+	"github.com/Mobi07/notification-stream-engine.git/pkg/logger"
+	"go.uber.org/zap"
+)
 
 func main() {
 	logger.Init()
 	defer logger.Sync()
+
+	producer, err := rabbitmq.NewProducer("amqp://guest:guest@localhost:5672/", "events_queue")
+	if err != nil {
+		logger.Log.Fatal("failed to create producer", zap.Error(err))
+	}
+
+	err = producer.Publish([]byte(`{"id":"1","type":"user_signup","timestamp":1627847261,"payload":{"user_id":"123"}}`))
+	if err != nil {
+		logger.Log.Error("failed to publish event", zap.Error(err))
+		return
+	}
 
 	logger.Log.Info("API service started")
 }
--- cmd/worker/main.go
+++ cmd/worker/main.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"github.com/Mobi07/notification-stream-engine.git/internal/broker/rabbitmq"
+	"github.com/Mobi07/notification-stream-engine.git/pkg/logger"
+	"go.uber.org/zap"
+)
+
+func main() {
+	logger.Init()
+	defer logger.Sync()
+
+	logger.Log.Info("Worker service started")
+	err := rabbitmq.StartConsumer("amqp://guest:guest@localhost:5672/", "events_queue")
+	if err != nil {
+		logger.Log.Fatal("failed to start consumer", zap.Error(err))
+	}
+}
